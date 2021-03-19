e009057237ec10425f960c6e1f9d713e807258fd
ashiph-ui <76753545+ashiph-ui@users.noreply.github.com>
2021-03-19 10:36:26 +0000

    Added SubPopulation Class for the start of the project

---
+++ SIMULATION.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+
+
+
+class SubPopulationSim(W, H, Pdeath, Precovery, Preinfiction, Ptravel, Pquarantine, cityname):
+	self.W = W
+	self.H = H
+	self.Pdeath = Pdeath
+	self.Precovery = Precovery
+	
+	
+
+
+
+
+
+
+
+
+
+
+
+
