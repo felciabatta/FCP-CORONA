95a883fb82cf83a1bf3e4dba1aaf796653dbf460
ashiph-ui <76753545+ashiph-ui@users.noreply.github.com>
2021-03-19 10:58:24 +0000

    Made corrections: __init__

--- SIMULATION.py
+++ SIMULATION.py
@@ -3,18 +3,28 @@ import numpy as np
 
 
 
-class SubPopulationSim(Width, Height, Pdeath, Pinfection, Precovery, Preinfection, Ptravel, Pquarantine, cityname):
-	self.Width = Width
-	self.Height = Height
-	self.Pdeath = Pdeath
-	self.Pinfection = Pinfection
-	self.Precovery = Precovery
-	self.Preinfection = Preinfection
-	self.Ptravel = Ptravel
-	self.Pquarantine = Pquarantine
-	self.cityname = cityname
-	self.day = 0
-	self.state = np.zeros([Width, Height], int)
+class SubPopulationSim:
+	def __init__(self, Width, Height, Pdeath, Pinfection, Precovery, Preinfection, Ptravel, Pquarantine, cityname):
+		self.Width = Width
+		self.Height = Height
+		self.Pdeath = Pdeath
+		self.Pinfection = Pinfection
+		self.Precovery = Precovery
+		self.Preinfection = Preinfection
+		self.Ptravel = Ptravel
+		self.Pquarantine = Pquarantine
+		self.cityname = cityname
+		self.day = 0
+		self.state = np.zeros([Width, Height], int)
+
+
+
+
+
+
+
+	
+
 
 
 
