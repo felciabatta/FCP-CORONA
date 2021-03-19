b93e59140c15197bcd96b6ae7755ef7aa93db52f
Edward Emeny <edwardemeny@Edwards-MacBook-Air.local>
2021-03-19 11:26:10 +0000

    UpdatePerson

--- SIMULATION.py
+++ SIMULATION.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import numpy.random as r
 
 
 
@@ -8,6 +8,42 @@ class SubPopulationSim(W, H, Pdeath, Precovery, Preinfiction, Ptravel, Pquaranti
 	self.H = H
 	self.Pdeath = Pdeath
 	self.Precovery = Precovery
+    
+    
+    
+    
+    
+    def UpdatePerson(self, i, j, state):
+        """update person after every single day putting them into a new state of the same date"""
+        status = state[i, j]
+        rand = r.random()
+        if status == 'S':
+            if rand < self.UpdatePinfection:
+                return 'I'
+            else:
+                return 'S'
+        elif status == 'I':
+            if rand < self.Pdeath:
+                return 'D'
+            elif rand < self.Precovery:
+                return recovered
+        elif status == 'R':
+            if rand < self.Preinfiction:
+                return 'I'
+            elif:
+                return 'R'
+        elif status == 'D':
+            return 'D' 
+        elif status == 'V'    
+            if rand < self.Preinfiction:
+                return 'I'
+            elif:
+                return 'R'
+        elif status == 0:
+            return 0 
+            
+
+
 	
 	
 
