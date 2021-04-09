afe8c468f1ff21a699a5b398a5e16b332b145fde
Edward Emeny <edwardemeny@Edwards-MacBook-Air.local>
2021-04-09 14:09:30 +0100

    adding Line animation start

--- ANIMATION.py
+++ ANIMATION.py
@@ -3,7 +3,7 @@
 #For grid animation 
 import numpy as np
 import matplotlib.pyplot as plt
-
+from matplotlib.animation import FuncAnimation 
 
 class GridAnimation:
    
@@ -20,20 +20,21 @@ class GridAnimation:
 
 
 class LineAnimation: 
+    """Creates a line animation showing the changes in Suceptable, Infected, Recovered and Dead people"""
     
-
-
-
-
-
-
-
-
-
-
-
-
-
+    #Writing in Psuedo Code until function is created
+    def __init__(self, data, axes, duration, line):
+        self.axes = axes
+        self.line = line
+        self.duration = duration
+        self.Suceptable = data.loc['Susceptible', 'Population']
+        self.Recovered = data.loc['Recovered', 'Population'] + data.loc['Vaccinated', 'Population']
+        self.Infected = data.loc[Infected', 'Population'] + data.loc['Quarantine', 'Population'] + data.loc['Travelled', 'Population']
+        self.Dead = data.loc['Dead', 'Population']
+        
+    def LineAnimate
+        
+    
 
 
 
