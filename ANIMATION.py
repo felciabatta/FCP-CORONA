3c4491c84925a4a6f4f2f409da05c213cc903f4a
Edward Emeny <edwardemeny@Edwards-MacBook-Air.local>
2021-04-09 14:27:55 +0100

    Adding update function

--- ANIMATION.py
+++ ANIMATION.py
@@ -20,21 +20,25 @@ class GridAnimation:
 
 
 class LineAnimation: 
-    """Creates a line animation showing the changes in Suceptable, Infected, Recovered and Dead people"""
+    """Creates a line animation showing the changes in Suceptible, Infected, Recovered and Dead people"""
     
     #Writing in Psuedo Code until function is created
     def __init__(self, data, axes, duration, line):
         self.axes = axes
         self.line = line
         self.duration = duration
-        self.Suceptable = data.loc['Susceptible', 'Population']
-        self.Recovered = data.loc['Recovered', 'Population'] + data.loc['Vaccinated', 'Population']
-        self.Infected = data.loc[Infected', 'Population'] + data.loc['Quarantine', 'Population'] + data.loc['Travelled', 'Population']
-        self.Dead = data.loc['Dead', 'Population']
+        self.Suceptible = [data.loc['Susceptible', 'Population']]
+        self.Recovered = [data.loc['Recovered', 'Population'] + data.loc['Vaccinated', 'Population']]
+        self.Infected = [data.loc['Infected', 'Population'] + data.loc['Quarantine', 'Population'] + data.loc['Travelled', 'Population']]
+        self.Dead = [data.loc['Dead', 'Population']]
         
-    def LineAnimate
+    def update(self, data):
+        self.Suceptible.append(data.loc['Susceptible', 'Population'])
+        self.Recovered.append(data.loc['Recovered', 'Population'] + data.loc['Vaccinated', 'Population'])
+        self.Infected.append(data.loc['Infected', 'Population'] + data.loc['Quarantine', 'Population'] + data.loc['Travelled', 'Population'])
+        self.Dead.append(data.loc['Dead', 'Population'])
         
-    
+    def 
 
 
 
