c5539b77bcb9b2ce1690b3df26986b370a3f0133
Barnpb <dz19637@bristol.ac.uk>
2021-04-15 10:35:06 +0100

    Commiting changes

--- ANIMATION.py
+++ ANIMATION.py
@@ -6,7 +6,26 @@ import matplotlib.pyplot as plt
 
 
 class GridAnimation:
-   
+  def init (self, axes, simulation):
+      self.axes=axes
+      self.simulation=simulation
+      
+      rgb_matrix = self.simulation.get_sim.gridState
+      self.image = self.axes.imshow(rgb_matrix)
+      self.axes.set_xticks([])
+      self.axes.set_yticks([])
+
+  def init(self):
+        return self.update(0)
+
+  def update(self, framenum):
+        day = framenum
+        rgb_matrix = self.simulation.get_rgb_matrix()
+        self.image.set_array(rgb_matrix)
+        return [self.image]
+        print(self.image)
+      
+        
        
 
 
@@ -14,12 +33,9 @@ class GridAnimation:
 
 
 
-
-
-
-
-
 class LineAnimation: 
+    
+        
     
 
 
